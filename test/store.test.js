7f909e8748f147feb7128f283f957c0bc56a76d2
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-07-30 16:34:10 -0700

    first test passes

---
+++ test/store.test.js
@@ -0,0 +1,22 @@
+import store from '../src/data/store.js'
+import dresses from '../src/data/dresses.js';
+
+const test = QUnit.test;
+
+QUnit.module('Data Store');
+
+store.storage = window.sessionStorage;
+
+QUnit.testStart(() => {
+    store.storage.clear();
+});
+
+test('get and save', (assert) => {
+    const key = 'dress';
+    const dress = { name: 'black dress' }
+
+    store.save(key, dress);
+    const got = store.get(key);
+
+    assert.deepEqual(got, dress);
+})
