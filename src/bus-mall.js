85bc80b1dc05be5c7f9755a01a8b77755db7df28
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-07-30 11:19:05 -0700

    done with layout

---
+++ src/bus-mall.js
@@ -0,0 +1,3 @@
+import dresses from './data/dresses.js';
+import store from './data/store.js';
+
--- src/data/dresses.js
+++ src/data/dresses.js
@@ -2,67 +2,67 @@ const dresses = [
     {
         id: 'casual-black-dress',
         name: 'Casual Black Dress',
-        image: '../assets/dress1.jpg'
+        image: 'assets/dress1.jpg'
     },
     {
         id: 'casual-black-red-dress',
         name: 'Casual Black and Red Dress',
-        image: '../assets/dress2.jpg'
+        image: 'assets/dress2.jpg'
     },
     {
         id: 'colorful-dress',
         name: 'Colorful Party Dress',
-        image: '../assets/dress3.jpg'
+        image: 'assets/dress3.jpg'
     },
     {
         id: 'summer-cocktail-dress',
         name: 'Summer Cocktail Dress',
-        image: '../assets/dress4.jpg'
+        image: 'assets/dress4.jpg'
     },
     {
         id: 'fancy-winter-dress',
         name: 'Fancy Winter Dress',
-        image: '../assets/dress5.jpg'
+        image: 'assets/dress5.jpg'
     },
     {
         id: 'floral-cocktail-dress',
         name: 'Floral Cocktail Dress',
-        image: '../assets/dress6.jpg'
+        image: 'assets/dress6.jpg'
     },
     {
         id: 'pink-cocktail-dress',
         name: 'Pink Cocktail Dress',
-        image: '../assets/dress7.jpg'
+        image: 'assets/dress7.jpg'
     },
     {
         id: 'black-gown-dress',
         name: 'Black Gown Dress',
-        image: '../assets/dress8.jpg'
+        image: 'assets/dress8.jpg'
     },
     {
         id: 'pink-gold-dress',
         name: 'Pink and Gold Dress',
-        image: '../assets/dress9.jpg'
+        image: 'assets/dress9.jpg'
     },
     {
         id: 'casual-blue-dress',
         name: 'Casual Blue Dress',
-        image: '../assets/dress10.jpg'
+        image: 'assets/dress10.jpg'
     },
     {
         id: 'fancy-black-pink-dress',
         name: 'Fancy Black and pink Dress',
-        image: '../assets/dress11.jpg'
+        image: 'assets/dress11.jpg'
     },
     {
         id: 'fancy-black-dress',
         name: 'Fancy BlackDress',
-        image: '../assets/dress12.jpg'
+        image: 'assets/dress12.jpg'
     },
     {
         id: 'pink-party-dress',
         name: 'Pink Party Dress',
-        image: '../assets/dress13.jpg'
+        image: 'assets/dress13.jpg'
     },
 ];
 
