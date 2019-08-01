cd5c119ae4667065f6fd19c1803a915967736792
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-08-01 09:27:43 -0700

    got some things working, still not done with the whole app

--- src/bus-mall.js
+++ src/bus-mall.js
@@ -1,4 +1,5 @@
 import store from './data/store.js';
+import dresses from './data/dresses.js';
 import DressCombination from './dress-combination.js';
 import { getRandom, findDress } from './util.js';
 import renderOutput from './output.js';
@@ -14,3 +15,15 @@ const percentageDisplay = document.getElementById('percentage');
 let turns = 0;
 let count = 0;
 
+const newDress = new DressCombination(dresses);
+
+
+let arr = [];
+
+for(let i = 0; i < 3; i++) {
+    const product = newDress.getRandomDress();
+    newDress.removeById(product.id);
+    arr.push(product);
+}
+const dressRender = renderDresses(arr[0], arr[1], arr[2]);
+dressChoices.appendChild(dressRender);
