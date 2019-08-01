6d2da872e30e33b7082f88f25eda0b29b798fa93
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-08-01 10:59:49 -0700

    chart with mock data displays

--- src/chart.js
+++ src/chart.js
@@ -1,16 +1,19 @@
 const ctx = document.getElementById('chart').getContext('2d');
 
 const data = [9, 20, 10, 4, 12, 6];
+const labelColors = ['black', 'pink', 'red', 'purple', 'blue', 'yellow'];
 
-const myChart = new myChart(ctx, {
+const clickChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: 'black',
-        datasets: [{
-            label: '# of Clicks',
-            data: data,
-            backgroundColor: 'white'
-        }]
+        labels: labelColors,
+        datasets: [
+            {
+                label: '# of Clicks',
+                data: data,
+                backgroundColor: 'white'
+            }
+        ]
     },
     options: {
         scales: {
