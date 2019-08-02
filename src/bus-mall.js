1bfcbc0888f706e9fa20c7c01d404b931c02f5b3
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-08-01 17:39:19 -0700

    added a turn function

--- src/bus-mall.js
+++ src/bus-mall.js
@@ -5,21 +5,34 @@ import { getRandom, findDress } from './util.js';
 import renderOutput from './output.js';
 import renderDresses from './dress-display.js';
 
-const dressInput = document.querySelectorAll('.dressInput');
-const radioInputs = document.querySelectorAll('input[name=dress');
+const dressInputs = document.querySelectorAll('.dressInput');
+const radioInputs = document.getElementsByClassName('dressy');
 const dressChoices = document.getElementById('img-div');
 const statistics = document.getElementById('click-data');
 const clickCount = document.getElementById('clicked');
 const percentageDisplay = document.getElementById('percentage');
 
-let turns = 0;
+let turns = 25;
 let count = 0;
 
+const products = store.getDresses();
 const newDress = new DressCombination(dresses);
 
-for(let i = 0; i < 3; i++) {
-    const product = newDress.getRandomDress();
-    newDress.removeById(product.id);
-    const dress = renderDresses(product);
-    dressChoices.appendChild(dress);
+showDresses();
+
+function showDresses() {
+    let tinyDressSet = newDress;
+    
+    for(let i = 0; i < 3; i++) {
+        const product = tinyDressSet.getRandomDress();
+        tinyDressSet.removeById(product.id);
+        const dress = renderDresses(product);
+        dressChoices.appendChild(dress);
+    }
+    
+    for(let i = 0; i < radioInputs.length; i++) {
+        radioInputs[i].addEventListener('click', () => {
+            turns--;
+        })
+    }
 }
